8159ec0f572237503a258c225c30561fcc8a94ee
El Sosi <cebollinos@gmail.com>
2024-06-22 12:30:47 +0200

    first commit

---
+++ card-utils.js
@@ -0,0 +1,42 @@
+
+const suitOrder = {
+    'clubs': 0,
+    'diamonds': 1,
+    'hearts': 2,
+    'spades': 3
+  };
+
+const rankOrder = {
+    'A':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10,
+    'J':11,
+    'Q':12,
+    'K':13,
+}
+
+export const suitImage = {
+    "spades": { symbol: "♠", color: "black" },
+    "hearts": { symbol: "♥", color: "red" },
+    "diamonds": { symbol: "♦", color: "red" },
+    "clubs": { symbol: "♣", color: "black" } 
+};
+
+
+ export function sortCardsBySuit(cards) {
+    return cards.sort((a, b) => {
+      return suitOrder[a.suit]*100+rankOrder[a.rank] - suitOrder[b.suit]*100-rankOrder[b.rank];
+    });
+  }
+
+export function sortHand()
+{
+    console.log("yo");
+}
---
+++ script.js
@@ -0,0 +1,101 @@
+
+//python -m http.server 8000
+
+import * as cardUtils from "./card-utils.js";
+
+    // script.js
+
+
+const playerHandElement = document.getElementById('player-hand');
+const dogCardsElement = document.getElementById('dog-cards');
+const dealButton = document.getElementById('deal-button');
+
+let deck = [];
+let playerHand = [];
+let dogHand = [];
+
+const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+const ranks = ['A','2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+// Initialize deck
+function initializeDeck() {
+    deck = [];
+    suits.forEach(suit => {
+        ranks.forEach(rank => {
+            deck.push({ suit, rank });
+        });
+    });
+    shuffleDeck();
+}
+
+// Shuffle deck
+function shuffleDeck() {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+}
+
+// Deal cards to the player
+function dealCards() {
+    var handsize = Math.floor(deck.length/4);
+    playerHand = deck.slice(0, handsize); // Deal 5 cards to player
+    playerHand = cardUtils.sortCardsBySuit(playerHand);
+    deck = deck.slice(handsize); // Remove dealt cards from deck
+
+    renderPlayerHand();
+}
+
+// Render player's hand
+function renderPlayerHand() {
+    cardUtils.sortHand();
+    playerHandElement.innerHTML = '';
+    playerHand.forEach((card, index) => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.classList.add(cardUtils.suitImage[card.suit].color);
+        cardElement.innerText = `${card.rank}\n${ cardUtils.suitImage[card.suit].symbol}`;
+        cardElement.addEventListener('click', () => tradeWithDog(index));
+        playerHandElement.appendChild(cardElement);
+    });
+}
+
+// Send card to dog
+function tradeWithDog(index) {
+    const playedCard = playerHand.splice(index, 1)[0];
+    dogHand.push(playedCard);
+    renderDog();
+    renderPlayerHand();
+}
+
+//send card from dog to hand
+function tradeWithHand(index) {
+    const playedCard = dogHand.splice(index, 1)[0];
+    playerHand.push(playedCard);
+    renderDog();
+    renderPlayerHand();
+}
+
+// Render played cards
+function renderDog() {
+    dogCardsElement.innerHTML = '';
+    dogHand.forEach((card, index) => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.classList.add(cardUtils.suitImage[card.suit].color);
+        cardElement.innerText = `${card.rank}\n${ cardUtils.suitImage[card.suit].symbol}`;
+        cardElement.addEventListener('click', () => tradeWithHand(index));
+        dogCardsElement.appendChild(cardElement);
+    });
+}
+
+// Event listener for dealing cards
+dealButton.addEventListener('click', () => {
+    initializeDeck();
+    dealCards();
+});
+
+// Initial render
+initializeDeck();
+renderPlayerHand();
+renderDog();
