8cced6a7f764c623fe57b81217afd22271f4e7db
El Sosi <cebollinos@gmail.com>
2024-06-24 21:54:04 +0200

    dog showing

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ import * as cardUtils from "./card-utils.js";
 
 
 const playerHandElement = document.getElementById('player-hand');
-const dogCardsElement = document.getElementById('dog-cards');
+const dogCardsElement = document.getElementById('dog-hand');
 const dealButton = document.getElementById('deal-button');
 
 let deck = [];
@@ -38,12 +38,31 @@ function shuffleDeck() {
 
 // Deal cards to the player
 function dealCards() {
-    var handsize = Math.floor(deck.length/4);
+
+    var handsize = Math.floor(deck.length/4-4);
     playerHand = deck.slice(0, handsize); // Deal 5 cards to player
     playerHand = cardUtils.sortCardsBySuit(playerHand);
     deck = deck.slice(handsize); // Remove dealt cards from deck
-
+    //deal to the dog
+    dogHand = deck.slice(0,4);
+    dogHand = cardUtils.sortCardsBySuit(dogHand);
+    deck = deck.slice(4);
     renderPlayerHand();
+}
+
+function renderCard(card)
+{
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.classList.add(cardUtils.suitImage[card.suit].color);
+    //cardElement.innerText = `${card.rank}\n${ cardUtils.suitImage[card.suit].symbol}`;
+    
+    // Create the img element
+    var imgElement = document.createElement('img');
+    imgElement.src = `cards/${card.rank}${ cardUtils.suitImage[card.suit].imageCode}.svg`;
+    cardElement.appendChild(imgElement);
+    
+    return cardElement;
 }
 
 // Render player's hand
@@ -51,39 +70,15 @@ function renderPlayerHand() {
     cardUtils.sortHand();
     playerHandElement.innerHTML = '';
     playerHand.forEach((card, index) => {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.classList.add(cardUtils.suitImage[card.suit].color);
-        //cardElement.innerText = `${card.rank}\n${ cardUtils.suitImage[card.suit].symbol}`;
+        
+        const cardElement = renderCard(card);
         //cardElement.addEventListener('click', () => tradeWithDog(index));
-        
-        // Create the img element
-        var imgElement = document.createElement('img');
-        imgElement.src = `cards/${card.rank}${ cardUtils.suitImage[card.suit].imageCode}.svg`;
-
-        // Append the img element to the card div
-        cardElement.appendChild(imgElement);
-
-        
         playerHandElement.appendChild(cardElement);
+
+        
     });
 }
 
-// Send card to dog
-function tradeWithDog(index) {
-    const playedCard = playerHand.splice(index, 1)[0];
-    dogHand.push(playedCard);
-    renderDog();
-    renderPlayerHand();
-}
-
-//send card from dog to hand
-function tradeWithHand(index) {
-    const playedCard = dogHand.splice(index, 1)[0];
-    playerHand.push(playedCard);
-    renderDog();
-    renderPlayerHand();
-}
 
 // Render played cards
 function renderDog() {
@@ -92,11 +87,12 @@ function renderDog() {
 
     dogCardsElement.innerHTML = '';
     dogHand.forEach((card, index) => {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.classList.add(cardUtils.suitImage[card.suit].color);
-        cardElement.innerText = `${card.rank}\n${ cardUtils.suitImage[card.suit].symbol}`;
-        cardElement.addEventListener('click', () => tradeWithHand(index));
+        const cardElement = renderCard(card);
+        // const cardElement = document.createElement('div');
+        // cardElement.classList.add('card');
+        // cardElement.classList.add(cardUtils.suitImage[card.suit].color);
+        // cardElement.innerText = `${card.rank}\n${ cardUtils.suitImage[card.suit].symbol}`;
+        // //cardElement.addEventListener('click', () => tradeWithHand(index));
         dogCardsElement.appendChild(cardElement);
     });
 
@@ -121,7 +117,21 @@ function renderDog() {
         }
 }
 
+// Send card to dog
+function tradeWithDog(index) {
+    const playedCard = playerHand.splice(index, 1)[0];
+    dogHand.push(playedCard);
+    renderDog();
+    renderPlayerHand();
+}
 
+//send card from dog to hand
+function tradeWithHand(index) {
+    const playedCard = dogHand.splice(index, 1)[0];
+    playerHand.push(playedCard);
+    renderDog();
+    renderPlayerHand();
+}
 
 // Initial render
 initializeDeck();
