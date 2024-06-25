534b1436f933c6aa5aec2ab5a01e2795003bf7bc
El Sosi <cebollinos@gmail.com>
2024-06-25 15:49:31 +0200

    more mvc

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,28 +1,24 @@
 // controller.js
 
 import * as _model from './model.js';
-
-import {
-    renderPlayerHand,
-    renderDog
-} from './view.js';
+import * as _view from './view.js';
 
 
 function tradeWithDog(index) {
     const card = _model.tradeCardFromPlayerToDog(index);
-    renderDog(_model.getDogHand(), tradeWithHand, card);
-    renderPlayerHand(_model.getPlayerHand(), tradeWithDog);
+    _view.renderDog(_model.getDogHand(), tradeWithHand, card);
+    _view.renderPlayerHand(_model.getPlayerHand(), tradeWithDog);
 }
 
 function tradeWithHand(index) {
     const card = _model.tradeCardFromDogToPlayer(index);
-    renderDog(_model.getDogHand(), tradeWithHand);
-    renderPlayerHand(_model.getPlayerHand(), tradeWithDog, card);
+    _view.renderDog(_model.getDogHand(), tradeWithHand);
+    _view.renderPlayerHand(_model.getPlayerHand(), tradeWithDog, card);
 }
 
 
 // Initial render
 _model.initializeDeck();
 _model.dealCards();
-renderPlayerHand(_model.getPlayerHand(), tradeWithDog);
-renderDog(_model.getDogHand(), tradeWithHand);
+_view.renderPlayerHand(_model.getPlayerHand(), tradeWithDog);
+_view.renderDog(_model.getDogHand(), tradeWithHand);
